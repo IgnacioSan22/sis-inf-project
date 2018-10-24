cf9580861bdf6811965e4fe8a40a4097195c4a1d
Víctor Fernández Melic <melic93@gmail.com>
2018-10-24 21:43:04 +0200

    Project structure improvements

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,20 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from config import Config
 from flask_sqlalchemy import SQLAlchemy
-#from flask_migrate import Migrate
 from flask_login import LoginManager
 import pymysql
 
 app = Flask(__name__)
 Bootstrap(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost:3306/sis-inf-project-db'
+from config import Config
+
 login = LoginManager(app)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
-#migrate = Migrate(app, db)
 login = LoginManager(app)
-#login.login_view = 'login'
 
 from app import routes, models
 
 db.create_all() # In case user table doesn't exists already. Else remove it.    
-db.session.commit() # This is needed to write the changes to database
+db.session.commit()
--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
+from app import app
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you will never guess'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_INFO') or 'mysql+pymysql://root@localhost:3306/sis-inf-project-db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
